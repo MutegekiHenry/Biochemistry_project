43cc45bbfd22fe2a645f241a3ad54d910645f9fd
MutegekiHenry <henrymutegeki117@gmail.com>
2020-02-26 03:32:39 -0500

    add help and version argp

--- fasta_to_kmers.c
+++ fasta_to_kmers.c
@@ -3,6 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const char *argp_program_version = "program-version 1.0";
+const char *argp_program_bug_address = "<lubegaritah@gmail.com>";
+
+/* Program documentation. */
+static char doc[] = "Some documentation";
+
+/* Our argument parser.  The options, parser, and
+   args_doc fields are zero because we have neither options or
+   arguments; doc and argp_program_bug_address will be
+   used in the output for ‘--help’, and the ‘--version’
+   option will print out argp_program_version. */
+static struct argp argp = { 0, 0, 0, doc };
+
 
 int main(int argc, char *argv[])
 {
